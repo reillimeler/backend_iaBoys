fac70770874a1b7f45b330dce62af829cfc5ee79
Chawder <rrorro596@gmail.com>
2026-04-23 20:39:15 -0400

    creado insumo.service.ts

---
+++ frontend/src/app/services/insumo.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class InsumoService {
+  private API_URL_CREATE = 'http://localhost:3000/api/insumos'; // POST
+
+  constructor(private http: HttpClient) {}
+
+  crearInsumo(datos: any): Observable<any> {
+    return this.http.post(this.API_URL_CREATE, datos);
+  }
+}
